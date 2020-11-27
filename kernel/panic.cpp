3d5089d880d6268d53cad6e00202e748eb80816f
Håkon Jordet <haakon.jordet@gmail.com>
2020-11-27 01:17:15 +0100

    panichandler: print out some registers on panic
    Not needed but it's fun :)

--- kernel/panic.cpp
+++ kernel/panic.cpp
@@ -3,7 +3,20 @@
 
 namespace kernel::panic {
 void panic() {
-  print::print("KERNEL PANIC");
+  u32 eax, ebx, ecx, edx;
+  __asm__("nop" : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx));
+  print::println("KERNEL PANIC");
+
+  print::print("eax: ");
+  print::number(eax);
+  print::print(", ebx: ");
+  print::number(ebx);
+  print::print(", ecx: ");
+  print::number(ecx);
+  print::print(", edx: ");
+  print::number(edx);
+  print::println("");
+
   volatile bool run = 1;
   while (run) {
   }
