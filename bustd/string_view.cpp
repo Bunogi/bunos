b565ebc9832a0c2e6d8899bed3eca89da0f483e9
Håkon Jordet <haakon.jordet@gmail.com>
2020-12-14 19:42:23 +0100

    Bustd: Add StringView class

---
+++ bustd/string_view.cpp
@@ -0,0 +1,21 @@
+#include "string_view.hpp"
+
+#include <string.h>
+
+namespace bustd {
+StringView::StringView(nullptr_t) : m_data(nullptr), m_length(0) {}
+StringView::StringView(const char *s) : m_data(s), m_length(strlen(s)) {}
+StringView::StringView(const char *s, usize length)
+    : m_data(s), m_length(length) {}
+
+const char *StringView::data_or(const char *s) const {
+  if (m_data == nullptr || m_length == 0) {
+
+    return s;
+  } else {
+    return m_data;
+  }
+}
+const char *StringView::data() const { return m_data; }
+usize StringView::length() const { return m_length; }
+} // namespace bustd
---
+++ bustd/string_view.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <bustd/stddef.hpp>
+
+namespace bustd {
+class StringView {
+public:
+  StringView(nullptr_t);
+  StringView(const char *s);
+  StringView(const char *s, usize length);
+
+  // Return s if this is null or empty.
+  const char *data_or(const char *s) const;
+  usize length() const;
+  const char *data() const;
+
+private:
+  const char *m_data;
+  usize m_length;
+};
+} // namespace bustd
