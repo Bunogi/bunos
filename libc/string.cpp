c741f68fcf75e6af70233446a45a18f018fa4ba3
Håkon Jordet <haakon.jordet@gmail.com>
2020-12-22 23:10:08 +0100

    libc: add strcmp and memcmp

--- libc/string.cpp
+++ libc/string.cpp
@@ -15,6 +15,14 @@ char *strcpy(char *dest, const char *src) {
   return static_cast<char *>(memcpy(dest, src, strlen(src)));
 }
 
+int strcmp(const void *lhs, const void *rhs) {
+  // TODO: implement min()
+  auto s1 = strlen(reinterpret_cast<const char *>(lhs));
+  auto s2 = strlen(reinterpret_cast<const char *>(rhs));
+  auto smallest = s1 < s2 ? s1 : s2;
+  return memcmp(lhs, rhs, smallest);
+}
+
 void *memcpy(void *dest, const void *src, size_t n) {
   for (size_t i = 0; i < n; i++) {
     *(static_cast<char *>(dest) + i) = *(static_cast<const char *>(src) + i);
@@ -28,4 +36,16 @@ void *memset(void *ptr, int value, size_t n) {
     buf[i] = static_cast<unsigned char>(value);
   }
   return ptr;
+}
+
+int memcmp(const void *lhs, const void *rhs, size_t count) {
+  const auto *lhs_s = reinterpret_cast<const char *>(lhs);
+  const auto *rhs_s = reinterpret_cast<const char *>(rhs);
+  for (size_t i = 0; i < count; i++) {
+    if (lhs_s[i] < rhs_s[i])
+      return -1;
+    if (lhs_s[i] > rhs_s[i])
+      return 1;
+  }
+  return 0;
 }
--- libc/string.h
+++ libc/string.h
@@ -8,8 +8,11 @@ extern "C" {
 
 size_t strlen(const char *);
 char *strcpy(char *dest, const char *src);
+int strcmp(const void *lhs, const void *rhs);
+
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *ptr, int value, size_t n);
+int memcmp(const void *lhs, const void *rhs, size_t count);
 
 #ifdef __cplusplus
 }
