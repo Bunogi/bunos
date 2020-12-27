82aa796812ebf3a7441dc76bafddd07dd510d68b
Håkon Jordet <haakon.jordet@gmail.com>
2020-12-27 22:48:56 +0100

    kernel: Make pure virtual function call error nicer

--- kernel/utils.cpp
+++ kernel/utils.cpp
@@ -1,4 +1,6 @@
 #include <bustd/assert.hpp>
 
 // Needed to make pure functions work
-extern "C" void __cxa_pure_virtual() { UNREACHABLE(); }
+extern "C" void __cxa_pure_virtual() {
+  KERNEL_PANIC("Called a pure virtual function");
+}
