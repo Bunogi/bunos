67fa9cd4ae0572860e7a021163196603a3bef9cf
Håkon Jordet <haakon.jordet@gmail.com>
2020-12-27 22:43:34 +0100

    bustd: Improve API of stringview

--- bustd/string_view.cpp
+++ bustd/string_view.cpp
@@ -1,12 +1,14 @@
 #include "string_view.hpp"
 
+#include <bustd/math.hpp>
 #include <string.h>
 
 namespace bu {
+StringView::StringView() : StringView(nullptr) {}
 StringView::StringView(nullptr_t) : m_data(nullptr), m_length(0) {}
 StringView::StringView(const char *s) : m_data(s), m_length(strlen(s)) {}
-StringView::StringView(const char *s, usize length)
-    : m_data(s), m_length(length) {}
+StringView::StringView(const char *s, const usize length)
+    : m_data(length == 0 ? nullptr : s), m_length(length) {}
 
 const char *StringView::data_or(const char *s) const {
   if (m_data == nullptr || m_length == 0) {
@@ -17,6 +19,9 @@ const char *StringView::data_or(const char *s) const {
   }
 }
 const char *StringView::data() const { return m_data; }
+const u8 *StringView::data_u8() const {
+  return reinterpret_cast<const u8 *>(m_data);
+}
 usize StringView::len() const { return m_length; }
 
 bool StringView::operator==(const StringView &other) const {
@@ -29,4 +34,17 @@ bool StringView::operator==(const StringView &other) const {
 bool StringView::operator!=(const StringView &other) const {
   return !(*this == other);
 }
+
+StringView StringView::first_part(const usize upto) const {
+  return StringView(m_data, upto);
+}
+
+StringView StringView::last_part(const usize from) const {
+  return StringView(m_data + from, m_length - from);
+}
+
+StringView StringView::substr(const usize from, const usize upto) const {
+  return StringView(m_data + from, bu::min(upto - from, m_length));
+}
+
 } // namespace bu
--- bustd/string_view.hpp
+++ bustd/string_view.hpp
@@ -5,14 +5,20 @@
 namespace bu {
 class StringView {
 public:
+  StringView();
   StringView(nullptr_t);
   StringView(const char *s);
-  StringView(const char *s, usize length);
+  StringView(const char *s, const usize length);
 
   // Return s if this is null or empty.
   const char *data_or(const char *s) const;
   usize len() const;
   const char *data() const;
+  const u8 *data_u8() const;
+
+  StringView first_part(const usize upto) const;
+  StringView substr(const usize from, const usize upto) const;
+  StringView last_part(const usize from) const;
 
   bool operator==(const StringView &other) const;
   bool operator!=(const StringView &other) const;
