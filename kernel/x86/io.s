6c1441b99b003c4ebbe37ef0b6525814d60be5bb
Håkon Jordet <haakon.jordet@gmail.com>
2021-01-09 01:05:04 +0100

    kernel: Use rep for out_u8_string

--- kernel/x86/io.s
+++ kernel/x86/io.s
@@ -6,19 +6,18 @@ _x86_out_u8_string:
 
     // do nothing on zero
     cmpl $0, %ecx
-    je 2f
+    je 1f
 
     // buffer
-    movl %esi, -4(%esp) // ABI: preserve esi 
+    movl %esi, -4(%esp) // ABI: preserve esi
     movl 8(%esp), %esi
     // port
     movw 4(%esp), %dx
 
     cld // increment esi instead of decrementing it
-1:
-    outsb
-    loop 1b
+    rep outsb
+    //loop 1b
 
     movl -4(%esp), %esi
-2:
+1:
     ret
