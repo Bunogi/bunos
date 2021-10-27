cbf223d6bfe5be02976f3a8e72b773b8ba7e1c8c
Håkon Jordet <haakon.jordet@gmail.com>
2021-10-27 20:07:47 +0200

    kernel: add stack protector with random guard
    Unfortunately, the instruction is super new so the code probably won't
    ever actually run, but it has been tested to work.

--- kernel/utils.cpp
+++ kernel/utils.cpp
@@ -1,6 +1,15 @@
 #include <bustd/assert.hpp>
+#include <kernel/panic.hpp>
 
 // Needed to make pure functions work
 extern "C" void __cxa_pure_virtual() {
   KERNEL_PANIC("Called a pure virtual function");
+}
+
+// This is intentionally in the kernel so that we can use libssp in GCC in
+// userspace.
+extern "C" {
+[[noreturn]] void __stack_chk_fail() {
+  KERNEL_PANIC("Stack smashing detected");
+}
 }
