3e8ed7603302992ed225b3dd72e68d858e256499
Håkon Jordet <haakon.jordet@gmail.com>
2021-10-30 21:17:17 +0200

    build: prevent rebuilding everything on each install

--- copy_libc_headers.sh
+++ copy_libc_headers.sh
@@ -4,16 +4,25 @@ set -euo pipefail
 typeset -r this_dir=$(dirname $(realpath $0))
 cd $this_dir
 
-rm -rf fsroot/usr/include
-
 dirs=$(cd libc && find . -type d)
 for i in $dirs; do
-    echo "Making fsroot/usr/include/$i" || true
-    mkdir -p fsroot/usr/include/$i
+    dir=fsroot/usr/include/$i
+    if [[ -f "$dir" ]]; then
+        echo "Not making $dir: already exists" || true
+        continue;
+    fi
+    echo "Making $dir" || true
+    mkdir -p $dir
 done
 
 headers=$(cd libc && find . -name '*.h')
 for i in $headers; do
+    source_file=libc/$i
+    dest_file=fsroot/usr/include/$i
+    if [ "$source_file" -ot "$dest_file" ]; then
+        echo "skipping $dest_file: not older than $source_file"
+        continue;
+    fi
     echo "libc/$i -> fsroot/usr/include/$i"
     cp libc/$i fsroot/usr/include/$i
 done
