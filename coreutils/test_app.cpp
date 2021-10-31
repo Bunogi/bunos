83d2e79c34f9a93e23e533e595a470a2a2e5191b
Håkon Jordet <haakon.jordet@gmail.com>
2021-10-31 12:21:19 +0100

    coreutils: make test_app a test app for reading from the keyboard

--- coreutils/test_app.cpp
+++ coreutils/test_app.cpp
@@ -1,5 +1,33 @@
+#include <fnctl.h>
 #include <stdio.h>
+#include <unistd.h>
 
-const char *name = "test";
+int main() {
+  size_t index = 0;
+  char buf[128];
 
-int main() { printf("Hello, world, my name is %s!\n", name); }
+  const int fd = open("/dev/keyboard", O_RDONLY);
+  if (fd < 0) {
+    perror("open");
+    return 1;
+  }
+
+  while (1) {
+    const int n = read(fd, buf + index, 1);
+    if (n < 1) {
+      perror("read");
+      break;
+    }
+    if (buf[index] == '\n') {
+      buf[index] = '\0';
+      printf("Hello %s, my name is world!\n", buf);
+      break;
+    } else {
+      index++;
+    }
+  }
+
+  if (close(fd)) {
+    perror("close");
+  }
+}
