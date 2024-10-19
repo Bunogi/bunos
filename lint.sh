e811ada8c09fe3e7fb9cb7a8d54addb493b5e547
Håkon Jordet <haakon.jordet@gmail.com>
2024-10-19 11:36:24 +0200

    meta: improve lint.sh

--- lint.sh
+++ lint.sh
@@ -1,4 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -x
+files=""
+
+while [ "$#" -ne 0 ]; do
+    if [ "$1" == '--' ]; then
+        shift;
+        break
+    fi
+    files="$files $1"
+    shift;
+done
+
+if [ "x$files" = "x" ]; then
+    files=$(git ls-files '*.cpp' '*.hpp' '*.h')
+fi
 
 ./compile_commands_target.sh
-clang-tidy $(git ls-files '*.cpp' '*.hpp' '*.h') $@
+clang-tidy $files $@
